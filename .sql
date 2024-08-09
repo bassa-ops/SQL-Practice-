a11ce81c71722a44bf6e0efe4d419f4c578eb746
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-08-09 16:24:51 +0530

    Update .sql

--- .sql
+++ .sql
@@ -1,5 +1,5 @@
-Show first name, last name and role of every person that is either patient or doctor.
-The roles are either "Patient" or "Doctor"
+/*Show first name, last name and role of every person that is either patient or doctor.
+The roles are either "Patient" or "Doctor"*/
 
 select first_name, last_name, 'patient' as role from patients
 union all
